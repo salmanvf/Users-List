c7855fc768824aebf6f106451820028d8b5d23fb
unknown <salman.ahmad@virtualforce.io>
2021-09-02 09:05:35 +0500

    initial

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+import Styles from "../styles/Test/Testing.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={Styles.footercls}>Copyright 2021 Ninja List</footer>
+  );
+};
+
+export default Footer;
---
+++ comps/Navbar.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.png" width={100} height={60} />
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Ninja Listing</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
